f2b1849d8bc827bafab91295b78e0eb7205c21c0
MAB MET <mabmet2023@gmail.com>
2024-11-09 11:47:13 +0700

    create footer.php

---
+++ include/footer.php
@@ -0,0 +1,86 @@
+<footer class="leading-loose xl:text-[1.1rem] mm:text-[1rem]">
+        <div class="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-20 bg-gradient-to-r from-[#1E6493] to-[#2B8EC3] text-white py-10">
+        <!-- Address Section -->
+        <div>
+            <h3 class="mb-3 font-semibold xl:text-xl">Address</h3>
+            <p class="font-sans">#73 St. BT-12M (Borey New World La Sen Sok) Krang Thnong Commune, Sen Sok District, Phnom Penh, Cambodia.</p>
+
+            <!-- Contact Section -->
+            <h3 class="mt-6 mb-3 font-semibold xl:text-xl">Contact</h3>
+            <p class="font-sans">(+855) 10 943 222<br>
+            (+855) 10 836 222<br>
+            <a href="https://www.milkywayschool.com" class="font-sans text-white hover:text-yellow-500 hover:underline">www.milkywayschool.com</a><br>
+            <a href="mailto:info@milkywayschool.com" class="font-sans text-white hover:text-yellow-500 hover:underline">info@milkywayschool.com</a>
+            </p>
+        </div>
+      
+          <!-- Programs Section -->
+          <div>
+            <h3 class="mb-3 font-semibold xl:text-xl">English General Program</h3>
+            <ul>
+              <li class="font-sans cursor-pointer hover:text-yellow-500 hover:underline">Kindergarten</li>
+              <li class="font-sans cursor-pointer hover:text-yellow-500 hover:underline">Primary School</li>
+              <li class="font-sans cursor-pointer hover:text-yellow-500 hover:underline">Lower Secondary School</li>
+              <li class="font-sans cursor-pointer hover:text-yellow-500 hover:underline">Secondary School</li>
+              <li class="font-sans cursor-pointer hover:text-yellow-500 hover:underline">Part-time Class</li>
+            </ul>
+            <h3 class="mt-6 mb-3 font-semibold xl:text-xl">Khmer General Program</h3>
+            <ul>
+              <li class="font-sans cursor-pointer hover:text-yellow-500 hover:underline">Kindergarten</li>
+              <li class="font-sans cursor-pointer hover:text-yellow-500 hover:underline">Primary School</li>
+              <li class="font-sans cursor-pointer hover:text-yellow-500 hover:underline">High School</li>
+            </ul>
+          </div>
+      
+          <!-- Useful Links & About Us -->
+          <div>
+            <h3 class="mb-3 font-semibold xl:text-xl">Useful Links</h3>
+            <ul>
+              <li><a href="#" class="font-sans text-white cursor-pointer hover:text-yellow-500 hover:underline">Home</a></li>
+              <li><a href="#" class="font-sans text-white cursor-pointer hover:text-yellow-500 hover:underline">Admission</a></li>
+              <li><a href="#" class="font-sans text-white cursor-pointer hover:text-yellow-500 hover:underline">News and Events</a></li>
+              <li><a href="#" class="font-sans text-white cursor-pointer hover:text-yellow-500 hover:underline">Campuses</a></li>
+            </ul>
+          </div>
+
+            <div>
+                <div>
+                    <h3 class="mb-3 font-semibold xl:text-xl">About Us</h3>
+                    <ul>
+                        <li><a href="#" class="font-sans text-white cursor-pointer hover:text-yellow-500 hover:underline">Vision and Mission</a></li>
+                        <li><a href="#" class="font-sans text-white cursor-pointer hover:text-yellow-500 hover:underline">Message From School President</a></li>
+                        <li><a href="#" class="font-sans text-white cursor-pointer hover:text-yellow-500 hover:underline">Why Milky Way School?</a></li>
+                        <li><a href="#" class="font-sans text-white cursor-pointer hover:text-yellow-500 hover:underline">Careers</a></li>
+                    </ul>
+                </div>
+
+
+                <!-- Visitors Info -->
+                <div class="mt-6">
+                    <h3 class="mb-3 font-semibold xl:text-xl">Visitors</h3>
+                    <p class="font-sans">Visit Today: 123</p>
+                    <p class="font-sans">Total Visit: 12345</p>
+                </div>
+            </div>
+        </div>
+      
+        <!-- Footer Bottom Section -->
+        <div class="py-6 bg-white md:px-20 mm:px-16">
+          <div class="container flex flex-col items-center justify-between mx-auto md:flex-row">
+            <!-- Copyright and Policies -->
+            <div class="flex lg:gap-7 md:gap-2 mm:flex-col mm:text-center lg:flex-row lg:text-center md:text-left"> 
+                <p class="xl:text-lg md:text-sm">© COPYRIGHT 2023. Milky Way School</p>
+                <p class="mt-4 xl:text-lg md:text-sm md:mt-0">Cookie Policy</p>
+                <p class="xl:text-lg md:text-sm">Privacy Policy</p>
+                <p class="xl:text-lg md:text-sm">Accessibility</p>
+            </div>
+            <div class="flex mt-4 xl:space-x-6 mm:space-x-4 md:mt-0">
+              <a href="#" class="text-gray-800 hover:text-[#1E6493]"><ion-icon class="w-8 h-8" name="logo-facebook"></ion-icon></a>
+              <a href="#" class="text-gray-800 hover:text-[#1E6493]"><ion-icon class="w-8 h-8" name="logo-instagram"></ion-icon></a>
+              <a href="#" class="text-gray-800 hover:text-[#1E6493]"><ion-icon class="w-8 h-8" name="logo-youtube"></ion-icon></a>
+              <a href="#" class="text-gray-800 hover:text-[#1E6493]"><ion-icon class="w-8 h-8" name="paper-plane-outline"></ion-icon></a>
+            </div>
+            
+          </div>
+        </div>
+    </footer>
